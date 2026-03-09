7e7560b266c10ce1f80eab69c9d30028431e76ef
hojat72elect <hojat72elect@gmail.com>
2026-03-09 01:55:10 -0400

    Migrated the game 2 to TypeScript.

--- Games2/src/scenes/GameOver.ts
+++ Games2/src/scenes/GameOver.ts
@@ -9,16 +9,19 @@ export class GameOver extends Scene {
         //  Get the current highscore from the registry
         const score = this.registry.get('highscore');
 
-        const textStyle = {fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff', stroke: '#000000', strokeThickness: 8};
+        const textStyle: Phaser.Types.GameObjects.Text.TextStyle = {
+            fontFamily: 'Arial Black',
+            fontSize: 64,
+            color: '#ffffff',
+            stroke: '#000000',
+            strokeThickness: 8
+        };
 
         this.add.image(512, 384, 'background');
-
         this.add.text(512, 300, `Game Over\n\nHigh Score: ${score}`, textStyle).setAlign('center').setOrigin(0.5);
 
         this.input.once('pointerdown', () => {
-
             this.scene.start('MainMenu');
-
         });
     }
 }
