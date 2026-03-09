b06a04ba47d03ab6ecc94ee03e5a1042e76fba64
hojat72elect <hojat72elect@gmail.com>
2026-03-09 01:36:11 -0400

    MainMenu.js -> ts

--- Games2/src/scenes/MainMenu.ts
+++ Games2/src/scenes/MainMenu.ts
@@ -9,7 +9,13 @@ export class MainMenu extends Scene {
         //  Get the current highscore from the registry
         const score = this.registry.get('highscore');
 
-        const textStyle = {fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff', stroke: '#000000', strokeThickness: 8};
+        const textStyle: Phaser.Types.GameObjects.Text.TextStyle = {
+            fontFamily: 'Arial Black',
+            fontSize: 38,
+            color: '#ffffff',
+            stroke: '#000000',
+            strokeThickness: 8
+        };
 
         this.add.image(512, 384, 'background');
 
@@ -24,7 +30,7 @@ export class MainMenu extends Scene {
 
         this.add.text(32, 32, `High Score: ${score}`, textStyle);
 
-        const instructions = [
+        const instructions: string[] = [
             'How many coins can you',
             'click in 10 seconds?',
             '',
@@ -34,9 +40,7 @@ export class MainMenu extends Scene {
         this.add.text(512, 550, instructions, textStyle).setAlign('center').setOrigin(0.5);
 
         this.input.once('pointerdown', () => {
-
             this.scene.start('ClickerGame');
-
         });
     }
 }
