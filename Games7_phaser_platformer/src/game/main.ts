5399d8a9d1d36485580ecf7ba4828c9cc1c7e2b2
hojat72elect <hojat72elect@gmail.com>
2026-03-14 16:11:39 -0400

    Added the barebones of the game 7.

---
+++ Games7_phaser_platformer/src/game/main.ts
@@ -0,0 +1,26 @@
+import { AUTO, Game, Scale,Types } from 'phaser';
+import Boot from "./scenes/Boot";
+import Preloader from "./scenes/Preloader";
+import MainMenu from "./scenes/MainMenu";
+import MainGame from "./scenes/Game";
+
+const config: Types.Core.GameConfig = {
+    type: AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container',
+    backgroundColor: '#008eb0',
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH
+    },
+    scene: [
+        Boot, Preloader, MainMenu, MainGame
+    ]
+};
+
+const StartGame = (parent: string) => {
+    return new Game({ ...config, parent });
+}
+
+export default StartGame;
