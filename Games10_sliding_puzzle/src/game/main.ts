8cb4ddc202eba4bcc0dd3737e53493f80ecb26ab
hojat72elect <hojat72elect@gmail.com>
2026-03-14 21:42:29 -0400

    Added the barebones of the game 10.

---
+++ Games10_sliding_puzzle/src/game/main.ts
@@ -0,0 +1,32 @@
+import {AUTO, Game, Scale, Types} from 'phaser';
+import {Intro, MineSweeper} from "./minesweeper";
+
+
+const config: Types.Core.GameConfig = {
+    type: AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: 0x2d2d2d,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {x: 0, y: 300},
+            debug: false
+        }
+    },
+    parent: 'game-container',
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH
+    },
+    scene: [
+        Intro, MineSweeper
+    ]
+};
+
+const StartGame = (parent: string) => {
+    return new Game({...config, parent});
+}
+
+export default StartGame;
